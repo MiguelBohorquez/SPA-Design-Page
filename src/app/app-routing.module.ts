888990ce6502bf282bada4d200b533c76f3d6d34
MiguelBohorquez <m.a.bohorquezduarte@gmail.com>
2021-05-16 20:29:15 +0200

    creando diseño de paginas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CaracteristicasComponent } from './component/caracteristicas/caracteristicas.component';
+import { ContactComponent } from './component/contact/contact.component';
+import { HomeComponent } from './component/home/home.component';
+import { NoFoundComponent } from './component/no-found/no-found.component';
+import { ServiciosComponent } from './component/servicios/servicios.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'pagina-principal',component: HomeComponent},
+  {path: 'caracteristicas', component: CaracteristicasComponent},
+  {path: 'servicios', component: ServiciosComponent},
+  {path: 'contactanos', component: ContactComponent},
+  {path: '**', component: NoFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,22 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './component/footer/footer.component';
 import { NavbarComponent } from './component/navbar/navbar.component';
 import { HomeComponent } from './component/home/home.component';
+import { CaracteristicasComponent } from './component/caracteristicas/caracteristicas.component';
+import { ServiciosComponent } from './component/servicios/servicios.component';
+import { NoFoundComponent } from './component/no-found/no-found.component';
+import { ContactComponent } from './component/contact/contact.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    CaracteristicasComponent,
+    ServiciosComponent,
+    NoFoundComponent,
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
